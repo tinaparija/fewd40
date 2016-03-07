924f75a588ec0b572b6b562301581d34c300f3c2
Tina Parija <tinaparija@gmail.com>
2016-03-06 18:15:41 -0800

    updating homework

--- MoodMusic/javascript/myjquery.js
+++ MoodMusic/javascript/myjquery.js
@@ -7,6 +7,6 @@ $(document).ready(function(){
 
 
 // User Clicks on Mood 
-// Mood Button blows up to be an audio player with album art and song information 
+// Mood Button blows up to be a large square audio player (all other buttons shift and shrink to the bottom )
 // Player randomly selects song from the mood's database 
 // Autoplays the song on click 
--- citipix-dropdown/javascript/myjquery.js
+++ citipix-dropdown/javascript/myjquery.js
@@ -3,18 +3,19 @@
 $(document).ready(function(){ 
 	 var cities = ['NYC', 'SF','LA','ATX','SYD'];
 	 var options = $('#city-type'); 
-	 $.each(cities, function (val, text) { // Loop to append options into the dropdown menu
+	 $.each(cities, function (index, city) { // Loop to append options into the dropdown menu
+	 		console.log(typeof index, typeof city);
 	 		options.append(
-	 				$('<option></option>').val(val).html(text)); 
+	 				$('<option></option>').val(index).html(city)); 
 	 }); 
 
 	$('#cityform').change(function(){  // Logs menu selection 
 		var selection = $('#cityform option:selected').val(); 	// Takes in User Selection 
 		if (selection === '0') { // If Else Function to update classes based on User Functions  
-		 	$('body').attr('class','nyc');}
-  	else if (selection === '1'){
-	 	 	$('body').attr('class','sf');}
-	 	else if (selection === '2'){
+		 	$('body').attr('class','nyc');
+		 } else if (selection === '1') {
+	 	 	$('body').attr('class','sf');
+	 	 } else if (selection === '2'){
 	 	 	$('body').attr('class','la');}
 	 	else if (selection === '3'){
 	 	 	$('body').attr('class','austin');}
